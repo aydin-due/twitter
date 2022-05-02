ae71175892abc44bd1bebdb8f26060aadf74e08f
aydin :^) <54159730+aydin-due@users.noreply.github.com>
2022-05-02 12:19:18 -0700

    s2r2

--- app/services/UserService.js
+++ app/services/UserService.js
@@ -4,6 +4,10 @@ class UserService {
     static create(id, username, name){
         return new User(id, username, name, "sin bio")
     }
+
+    static getInfo(user){
+        return [user.id, user.username, user.name, user.bio]
+    }
 }
 
 module.exports = UserService
--- test/services/UserService.test.js
+++ test/services/UserService.test.js
@@ -7,5 +7,13 @@ describe('Test for UserService', () => {
         expect(user.username).toBe("aydin-due")
         expect(user.name).toBe("aydin")
         expect(user.bio).not.toBeUndefined()
+    }),
+    test("2. Get user data in a list", () => {
+        const user = UserService.create(1, "aydin-due", "aydin")
+        const userInfoList = UserService.getInfo(user)
+        expect(userInfoList[0]).toBe(1)
+        expect(userInfoList[1]).toBe("aydin-due")
+        expect(userInfoList[2]).toBe("aydin")
+        expect(userInfoList[3]).toBe("sin bio")
     })
 })
