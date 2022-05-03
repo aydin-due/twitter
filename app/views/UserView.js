80903f4771e343fe5bf4a3c6c7d0fdc70393e272
aydin :^) <54159730+aydin-due@users.noreply.github.com>
2022-05-02 17:59:28 -0700

    s2 r2+3+4

--- app/views/UserView.js
+++ app/views/UserView.js
@@ -7,6 +7,15 @@ class UserView{
         if(payload=== null){
             return result = {error:"payload no existe"}
         }
+        else if(typeof payload.username !== 'string' || typeof payload.name !=='string' || typeof payload.id !=='number'){
+            return result = {error:"necesitan tener un valor válido"}
+        }
+        else if(payload.id === undefined || payload.username  === undefined || payload.name  === undefined){
+            return result = {error:"necesitan tener un valor válido"}
+        }
+        else {        
+            return UserService.create(payload.id, payload.username, payload.name)
+        }
     }
 }
 
--- test/views/UserView.test.js
+++ test/views/UserView.test.js
@@ -1,9 +1,26 @@
 const UserView = require('./../../app/views/UserView')
 
 describe('Tests for UserView', () => {
-    test("Return an error object when trying to create a user with a null payload", () => {
+    test("Return an error object when trying to create an user with a null payload", () => {
         const payload = null
         const result = UserView.createUser(payload)
         expect(result.error).toMatch(/payload no existe/)
+    }),
+    test("Return an error object when trying to create an user with a payload with invalid properties", () => {
+        const payload = {username:null, name:12, id:"id"}
+        const result = UserView.createUser(payload)
+        expect(result.error).toMatch(/necesitan tener un valor válido/)
+    }),
+    test("Return an error object when trying to create an user with a payload with missing properties", () => {
+        const payload = {username:"username"}
+        const result = UserView.createUser(payload)
+        expect(result.error).toMatch(/necesitan tener un valor válido/)
+    }),
+    test("Create user by a given valid payload", () => {
+        const payload = {username:"username", id:1, name:"name"}
+        const result = UserView.createUser(payload)
+        expect(result.name).toBe("name")
+        expect(result.username).toBe("username")
+        expect(result.id).toBe(1)
     })
 })
